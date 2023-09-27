6462e81641c85893a3911120eae5321b6fdc91ed
AdrianScharff <adrian18juarez@gmail.com>
2023-09-27 16:37:59 -0600

    Create for project

---
+++ app.js
@@ -0,0 +1,129 @@
+/* // Ejercicio 1
+for(let i = 1; i <= 50; i++) {
+    if (i%2 !== 0) {
+        console.log(i);
+    }
+}
+
+// Ejercicio 2
+const pokemons = 
+[ 'bulbasaur',
+  'ivysaur',
+  'venusaur',
+  'charmander',
+  'charmeleon',
+  'charizard',
+  'squirtle',
+  'wartortle',
+  'blastoise',
+  'caterpie',
+  'metapod',
+  'butterfree',
+  'weedle',
+  'kakuna',
+  'beedrill',
+  'pidgey',
+  'pidgeotto',
+  'pidgeot',
+  'rattata',
+  'raticate',
+  'spearow',
+  'fearow',
+  'ekans',
+  'arbok',
+  'pikachu',
+  'raichu',
+  'sandshrew',
+  'sandslash',
+  'nidoran-f',
+  'nidorina',
+  'nidoqueen',
+  'nidoran-m',
+  'nidorino',
+  'nidoking',
+  'clefairy',
+  'clefable',
+  'vulpix',
+  'ninetales',
+  'jigglypuff',
+  'wigglytuff',
+  'zubat',
+  'golbat',
+  'oddish',
+  'gloom',
+  'vileplume',
+  'paras',
+  'parasect',
+  'venonat',
+  'venomoth',
+  'diglett',
+  'dugtrio',
+  'meowth',
+  'persian',
+  'psyduck',
+  'golduck',
+  'mankey',
+  'primeape',
+  'growlithe',
+  'arcanine',
+  'poliwag',
+  'poliwhirl',
+  'poliwrath',
+  'abra',
+  'kadabra',
+  'alakazam',
+  'machop',
+  'machoke',
+  'machamp',
+  'bellsprout',
+  'weepinbell',
+  'victreebel',
+  'tentacool',
+  'tentacruel',
+  'geodude',
+  'graveler',
+  'golem',
+  'ponyta',
+  'rapidash',
+  'slowpoke',
+  'slowbro',
+  'magnemite',
+  'magneton',
+  'farfetchd',
+  'doduo',
+  'dodrio',
+  'seel',
+  'dewgong',
+  'grimer',
+  'muk',
+  'shellder',
+  'cloyster',
+  'gastly',
+  'haunter',
+  'gengar',
+  'onix',
+  'drowzee',
+  'hypno',
+  'krabby',
+  'kingler',
+  'voltorb' ]
+
+const num = parseInt(prompt('Ingresa un numero'));
+
+for (let i = 0; i <= num; i++) {
+    if (i % 5 === 0 && i !== 0) {
+        console.log(pokemons[i - 1]);
+    }
+}
+
+
+// Ejercicio 3
+const array = [4,'dos',8,'tres',5,9,1,'cero'];
+
+array.forEach((item) => {
+    if (!isNaN(item)) {
+        console.log(item);
+    }
+});
+
+ */
